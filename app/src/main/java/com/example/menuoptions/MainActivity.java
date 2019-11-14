2c274579c1e3dcc9f4129a27f500a0bae7ce6640
Hadarmalul <hm6267@bs.amalnet.k12i.il>
2019-11-14 15:57:27 +0200

    done

--- app/src/main/java/com/example/menuoptions/MainActivity.java
+++ app/src/main/java/com/example/menuoptions/MainActivity.java
@@ -9,20 +9,39 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
+/**
+ * @author HadarMalul
+ * @since 14.11.19
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout LL;
+
+    /**
+     * LL is a Linear Layout java component
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         LL = (LinearLayout) findViewById(R.id.LL);
+
+        /**
+         * connecting between the java component to the xml component
+         */
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
+
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+
+        /**
+         * creating the Option Menu
+         */
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
@@ -37,11 +56,19 @@ public class MainActivity extends AppCompatActivity {
             LL.setBackgroundColor(Color.RED);
         }
         return true;
+
+        /**
+         *  checking the color on the chosen item and changing the background according to the color
+         */
     }
 
     public void next(View view) {
         Intent si = new Intent(this, anotherColor.class  );
         startActivity(si);
+
+        /**
+         *  screen moves to the next activity
+         */
 
     }
 }
--- app/src/main/java/com/example/menuoptions/anotherColor.java
+++ app/src/main/java/com/example/menuoptions/anotherColor.java
@@ -10,22 +10,40 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class anotherColor extends AppCompatActivity {
-        LinearLayout LL2;
+
+    LinearLayout LL2;
+
+    /**
+     * LL2 is a Linear Layout java component
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another_color);
+
         LL2 = (LinearLayout) findViewById(R.id.LL2);
+
+        /**
+         * connecting between the java component to the xml component
+         */
     }
-    public boolean onCreateOptionMenu(Menu menu){
+
+    public boolean onCreateOptionsMenu(Menu menu){
+
         getMenuInflater().inflate(R.menu.main, menu);
         menu.add(0,0,400,"Black");
         return true;
+
+        /**
+         * creating the Option Menu
+         * adding another item (Black)
+         */
     }
 
 
     public boolean onOptionsItemSelected(MenuItem item){
+
         String st = item.getTitle().toString();
         if (st.equals("Gray")){
             LL2.setBackgroundColor(Color.GRAY);
@@ -40,9 +58,18 @@ public class anotherColor extends AppCompatActivity {
             LL2.setBackgroundColor(Color.BLACK);
         }
         return true;
+
+        /**
+         *  checking the color on the chosen item and changing the background according to the color
+         */
     }
 
     public void back(View view) {
         finish();
+
+        /**
+         *  returns to the main activity
+         */
     }
+
 }
